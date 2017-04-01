c4d4b58a716762d072d589e487c47926fd0a873c
itaydafna <itaydafna@gmail.com>
2017-04-01 15:22:07 +0300

    lesson 11

--- src/App.js
+++ src/App.js
@@ -2,45 +2,33 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Route,
-    Link,
-    Switch
+    Link
 } from 'react-router-dom';
 
 import './App.css'
 
-const Links = () => (
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Header />
-        <Content />
-    </nav>
-)
-
-const Header = ({match}) => (
-    <div className="header">
-        <Route exact path="/:page?"
-            render={({match}) =>
-                (<h1>{match.params.page} header</h1>)} />
-    </div>)
-
-const Content = ({match}) => (
-    <div className="content">
-        <Route exact path="/:page?"
-            render={({match}) =>
-                (<p>{match.params.page} content</p>)} />
+const Home = () => (<h1>Home</h1>)
+const Menu = () =>
+    (<div>
+        <h1>Menu</h1>
+        <Link to="/menu/food">Food</Link>
+        <Link to="/menu/drinks">Drinks</Link>
+        <Link to="/menu/sides">Sides</Link>
+        <Route 
+        path="/menu/:section" 
+        render={({match})=><h2>{match.params.section}</h2>}/>
     </div>
-)
+    )
 
-class App extends React.Component {
-    render() {
-        return (<Router>
-            <div>
-                <Links />
-            </div>
-        </Router>
-        );
-    }
-}
+const App = (props) => (
+    <Router>
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Route exact path="/" component={Home} />
+            <Route path="/menu" component={Menu} />
+        </div>
+    </Router>
+)
 
 export default App
