ec74c7a0aabf773946ffcbeb926d46fe55994c91
itaydafna <itaydafna@gmail.com>
2017-04-01 16:09:17 +0300

    lesson 13

--- src/App.js
+++ src/App.js
@@ -3,46 +3,38 @@ import {
     BrowserRouter as Router,
     Route,
     Link,
-    Switch,
-    Redirect
+    Prompt
 } from 'react-router-dom';
 
 import './App.css'
 
 const loggedin = true;
 
-const Links = () =>
-    (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/old/123">Old</Link>
-            <Link to="/new/456">New</Link>
-            <Link to="/protected">Protected</Link>
-        </nav>
-    )
+const Home = () => (<h1>Home</h1>)
+class Form extends React.Component {
+    state = { dirty: false }
+    setDirty = () => this.setState({ dirty: true })
+    render() {
+        return (
+            <div>
+                <h1>Form</h1>
+                <input type="text" onInput={this.setDirty} />
+                <Prompt
+                when={this.state.dirty}
+                message = "Data will be lost!"
+                />
+            </div>
+        )
+    }
+}
 
 const App = (props) => (
     <Router>
         <div>
-            <Links />
-            {/*<Switch>*/}
-            <Route exact path="/" render={() => (<h1>Home</h1>)} />
-            <Route
-                exact path="/new/:str"
-                render={({match}) => (<h1>New: {match.params.str}</h1>)} />
-            <Route
-                path='/old/:str'
-                render={({match}) => (<Redirect to={`/new/${match.params.str}`} />)}
-            />
-            <Route
-                path='/protected'
-                render={() => (
-                    loggedin
-                        ? <h1>Welcome to the protected page!</h1>
-                        : <Redirect to={`/new/Login`} />)}
-            />
-            {/*<Redirect from="/old" to="/new" />*/}
-            {/*</Switch>*/}
+            <Link to="/">Home</Link>
+            <Link to="/form">Form</Link>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/form" component={Form} />
         </div>
     </Router>
 )
